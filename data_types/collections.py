c47cc7eec13ce725814649a77f50f5d8c1994a9b
Remigiusz Otocki <remigiusz.otocki@gmail.com>
2018-01-24 16:24:57 +0100

    Collections updated.

--- data_types/collections.py
+++ data_types/collections.py
@@ -4,7 +4,7 @@
 
 # Definition of a list for different types:
 empty_list = []  # This can also be achieved by using: empty_list = list()
-list_of_items = [1, 2, 3]  # list of ints (integers)
+list_of_ints = [1, 2, 3]  # list of ints (integers)
 list_of_floats = [1.2, 4.3]  # list of floats
 list_of_complex_values = [1+2j]  # list of complex's
 
@@ -29,5 +29,19 @@ tuple_of_different_types = (2, 3, 4.1, 'Cheers', [4, 3], (2,))
 
 # Items in the list can be accessed through subscript notation, like this:
 # some_list[index] - where index is an int not greater than the total amount of items in the list
-assert list_of_ints[0] == 1  # First item
+assert list_of_ints[0] == 1  # First item, index: zero
+assert list_of_ints[1] == 2  # Second item, index: one
+assert list_of_ints[2] == 3  # Third item, index: two
+# We need to stop here or else we'll try to access an item that is out of bounds.
+# This will cause the program to crash with this message: "IndexError: list index out of range".
+# You can try it out by uncommenting the line below:
+# list_of_ints[3]  # uncomment me, I dear you!
 
+# Ok, so we now know how to access the items in the collection and we are aware that this is a little bit dangerous
+# if we don't know the size of the collection which items we are trying to access.
+# We can check the size of the collection by calling len function on it:
+len(list_of_ints)
+len(tuple_of_ints)
+# We just need to remember that we can access all of the items for indexes equal or greater than 0
+# but at the same time the index needs to be less than len(some_collection) where some_collection is the collection
+# which items we are trying to access.
